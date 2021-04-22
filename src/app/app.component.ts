27110e6468f7f0f8f4d00430a150c68f7e9f7022
Joseph Ng <joseph_sieang@live.com>
2021-04-22 08:55:06 +0800

    Create production report chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fe-google-chart';
+  chart = {
+    title: '',
+    type: 'ColumnChart',
+    data: [
+      ['2020/4/6', 8136000, 19500000],
+      ['2020/4/7', 8538000, 3470000],
+      ['2020/4/8', 2244000, 2244000],
+      ['2020/4/9', 3470000, 8538000],
+      ['2020/4/10', 19500000, 8136000],
+
+    ],
+    columnNames: ['Date', 'Gyro', 'Palau'],
+    dynamicResize: true,
+    options: {
+      width: 600,
+      height: 300,
+      chartArea: {left: 200, bottom: 130, top: 30, width:'100%',height:'100%'},
+      hAxis: {
+        title: 'Date',
+        slantedText: true,
+        slantedTextAngle: 90
+      },
+      vAxis: {
+        // https://stackoverflow.com/questions/28327342/position-of-vertical-axis-title-in-google-charts to solve: google chart vAxis title being cut off and needs to move closer to axis
+        title: '\n\n\nCurrent production',
+        // gridlines: { count: 3 }, 
+        // minValue: 500
+      },
+      colors: ['#00EAD0', '#814EFA']
+    }
+  }
 }
