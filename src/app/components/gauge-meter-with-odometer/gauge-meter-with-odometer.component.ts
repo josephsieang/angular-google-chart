559cf7c75c366878e01a1fefce721eeaeaa10ce5
Joseph Ng <joseph_sieang@live.com>
2021-04-27 14:03:52 +0800

    Make animation duration depends on value

--- src/app/components/gauge-meter-with-odometer/gauge-meter-with-odometer.component.ts
+++ src/app/components/gauge-meter-with-odometer/gauge-meter-with-odometer.component.ts
@@ -12,7 +12,7 @@ export class GaugeMeterWithOdometerComponent implements OnInit {
   prevPercentage: number = 0;
   curPercentage: number = 0;
   percentage: number = 0;
-  lastPercentageAnimationClass = 'realtime-progress';
+  progressAnimationLength: string = '';
 
   constructor(private renderer: Renderer2) {}
 
@@ -24,19 +24,40 @@ export class GaugeMeterWithOdometerComponent implements OnInit {
     this.renderer.setStyle(this.realTimeProgress.nativeElement, '--lastNumerator', String(prevNumerator), 2);
     this.renderer.setAttribute(this.realTimeProgress.nativeElement, 'stroke-dasharray', `${prevNumerator}, 1413.75`);
 
-    this.curPercentage = this.prevPercentage + 2;
+    this.curPercentage = this.prevPercentage + 80;
     this.wholeNumber = Math.round(this.curPercentage);
     const curNumerator = (this.curPercentage * 1413.75) / 100;
 
-    this.renderer.removeClass(this.realTimeProgress.nativeElement, this.lastPercentageAnimationClass);
-    setTimeout(() => {
-      this.renderer.addClass(this.realTimeProgress.nativeElement, this.lastPercentageAnimationClass);
+    switch(true) {
+      case this.curPercentage <= 20:
+        this.progressAnimationLength = '0.8s';
+        break;
+      case this.curPercentage <= 40:
+        this.progressAnimationLength = '1.2s';
+        break;
+      case this.curPercentage <= 60:
+        this.progressAnimationLength = '1.6s';
+        break;
+      case this.curPercentage <= 80:
+        this.progressAnimationLength = '2.0s';
+        break;
+      case this.curPercentage <= 100:
+        this.progressAnimationLength = '2.4s';
+        break;
+      default:
+        this.progressAnimationLength = '0.8s';
+    }
+
+    this.renderer.removeClass(this.realTimeProgress.nativeElement, 'realtime-progress');
+    setTimeout(() => {this.renderer.setStyle(this.realTimeProgress.nativeElement, '--progressAnimationLength', String(3), 2);
+      this.renderer.addClass(this.realTimeProgress.nativeElement, 'realtime-progress');
+      this.renderer.setStyle(this.realTimeProgress.nativeElement, '--progressAnimationLength', this.progressAnimationLength, 2);
       this.renderer.setStyle(this.realTimeProgress.nativeElement, '--curNumerator', String(curNumerator), 2);
       this.renderer.setAttribute(this.realTimeProgress.nativeElement, 'stroke', '#814EFA');
     }, 200);
 
     setTimeout(() => {
       this.updateProgress();
-    }, 3000);
+    }, 10000);
   }
 }
